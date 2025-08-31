559866092ddbd100163fe5d6efafa77ae720112d
Chris <cgkocur@gmail.com>
2025-08-31 13:21:45 -0400

    moved dev environment to new class system

--- newServer.js
+++ newServer.js
@@ -28,5 +28,27 @@ NewGame.app.get('/board', (req, res) => {
 // Spin up the server
 NewGame.server.listen(3000, () => {
     const addressInfo = NewGame.server.address()
-    NewGame.generateQRCodeForServer(addressInfo.port)
+    NewGame.generateQRCodeForServer(addressInfo.port, 'player')
 })
+
+// Board Stuff
+const { Board } = require("./classes/Board")
+
+let gameBoard = new Board("Message To Myself", "Congrats are in order")
+
+gameBoard.board.forEach(row => console.table(row))
+console.log(gameBoard)
+
+// Player Stuff
+
+const { Player } = require('./classes/Player')
+const { v4: makeID } = require('uuid') //For Making Unique IDs
+
+// Wheel Stuff
+
+const { Wheel } = require('./classes/Wheel.js')
+
+let wheeltest = new Wheel()
+
+// wheeltest.generateSections(24, 2000)
+// console.log(wheeltest)
