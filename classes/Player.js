95f2d2fbdd82d271aa1d2970c15f6c8b19a43126
Chris <cgkocur@gmail.com>
2025-09-01 22:16:10 -0400

    added function doc

--- classes/Player.js
+++ classes/Player.js
@@ -10,15 +10,31 @@ class Player{
         this.score = 0
         this.color = ''
     }
+    /**
+     * Increments the score for player.
+     * @param {number} number Amount to add to score, include "-" to reduce score
+     */
     updateScore(number){
         this.score += number
     }
+    /**
+     * Sets score to a specific number.
+     * @param {number} number Amount to set score to.
+     */
     setScore(number){
         this.score = number
     }
+    /**
+     * Assigns a hex code to the player for the board to render a color.
+     * @param {string} hex A hexadecimal code for a color, provide by player client.
+     */
     setColor(hex){
         this.color = hex
     }
+    /**
+     * Set a name for the player for the board client to display.
+     * @param {string} string 
+     */
     setName(string){
         this.name = string
     }
