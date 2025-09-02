a15ca4b0cf444a5b93424926740a1df35d4ea160
Chris <cgkocur@gmail.com>
2025-09-01 22:17:39 -0400

    combined all the features into the class

--- classes/WOFGame.js
+++ classes/WOFGame.js
@@ -1,10 +1,20 @@
 const {Board} = require('./Board')
+const {Wheel} = require('./Wheel')
 const {PlayerHandler} = require('./PlayerHandler')
 
 
 class WOFGame{
-    constructor(){
-
+    /**
+     * If no values are provided it makes a default game. Otherwise you can supply values to create a game from a previous game state.
+     * @param {string} clue Clue for the puzzle
+     * @param {string} phrase Phrase for the puzzle
+     * @param {Array} sections Sections of the wheel array, mix of numbers and strings
+     * @param {Player[]} players Array of players
+     */
+    constructor(clue, phrase, sections, players){
+        this.Board = new Board(clue, phrase)
+        this.Wheel = new Wheel(sections)
+        this.PlayerHandler = new PlayerHandler(players)
     }
 }
 
