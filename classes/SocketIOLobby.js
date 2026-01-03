925d9be667537f3d35b94007b2a3a103c39d5d08
Chris <cgkocur@gmail.com>
2026-01-03 01:31:20 -0500

    created a template for an eventual lobby system addition to make later

---
+++ classes/SocketIOLobby.js
@@ -0,0 +1,17 @@
+import { Socket } from "socket.io";
+
+class SocketIOLobby extends Socket{
+    constructor(lobbyName){
+        this.name = lobbyName
+    }
+        /* 
+    TODO: Put event handlers built into a class
+    Add Connection Function
+    Add a disconnection function
+    Add a function to create a lobby.
+    Add a function to create namespaces
+    Add a function to send only to players
+    Add a function to send only to lobby/server
+    */
+
+}
