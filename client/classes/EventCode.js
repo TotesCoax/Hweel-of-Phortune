560dfb69a20a0d1823b9699ae54b4f48bfac94b8
Chris <cgkocur@gmail.com>
2026-01-03 01:29:33 -0500

    created an enum to make remembering event codes easier

---
+++ client/classes/EventCode.js
@@ -0,0 +1,14 @@
+export const EventCode = Object.freeze({
+    //General Codes
+    connection: "connection",
+    disconnect: "disconnect",
+    //Board Codes
+    boardJoin: "boardJoin",
+    gamestateRequest: "gamestateRequest",
+    //Player Codes
+    playerJoin: "playerJoin",
+    speedData: "speedData",
+    nameChange: "nameChange",
+    colorChange: "colorChange",
+    letterSubmission: "letterSubmission"
+})
